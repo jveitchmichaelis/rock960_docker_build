0051d05dd078852b0d5ba1a7145d04322795d7e5
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2019-03-25 22:45:17 +0000

    add extra post-install script

---
+++ mk-rootfs-extra.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -e
+
+# Directory contains the target rootfs
+TARGET_ROOTFS_DIR="binary"
+
+if [ ! $ARCH ]; then
+        ARCH='arm64'
+fi
+if [ ! $VERSION ]; then
+        VERSION="debug"
+fi
+
+if [ ! -e linaro-stretch-alip-*.tar.gz ]; then
+        echo "\033[36m Run mk-base-debian.sh first \033[0m"
+fi
+
+echo -e "\033[36m Change root.....................\033[0m"
+sudo mount -o bind /dev $TARGET_ROOTFS_DIR/dev
+
+cat <<EOF | sudo chroot $TARGET_ROOTFS_DIR
+
+#--------------ssh keys------------
+ssh-keygen -A
+
+#--------------Docker-------------
+curl -sSL https://get.docker.com | sh
+usermod -aG docker linaro
+
+#--------------Disable WiFi power management-------------
+touch /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
+echo "[connection]\nwifi.powersave = 2\n" > /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
+
+EOF
+
+sudo umount $TARGET_ROOTFS_DIR/dev
